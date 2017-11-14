2816e6a337cf3397170a1079923f2d8a59a74bb6
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-14 11:53:51 +0400

    name of the application updated

--- deploy/up.sh
+++ deploy/up.sh
@@ -5,16 +5,16 @@ export PORT_INSIDE=$2
 echo ">>>> Moving to $(dirname "$0")"
 cd "$(dirname "$0")"
 echo ">>>> Building docker image"
-docker build -t game_of_life:latest $PWD/..
+docker build -t docker_silex_skeleton:latest $PWD/..
 
 echo ">>>> Installing dependencies"
-composer install --prefer-dist --no-interaction --ignore-platform-reqs --working-dir=$PWD/../www
+php $PWD/../www/composer.phar install --prefer-dist --no-interaction --ignore-platform-reqs --working-dir=$PWD/../www
 
 echo ">>>> Removing old container"
 docker rm -f game_of_life || true
 
 echo ">>>> Running new container"
-docker run --name game_of_life -e APP_ENV=local -e APP_DEBUG=TRUE -e PORT=$PORT_INSIDE -d -p $PORT_OUTSIDE:$PORT_INSIDE -v $PWD/../www:/var/www/html game_of_life:latest
+docker run --name docker_silex_skeleton -e APP_ENV=local -e APP_DEBUG=TRUE -e PORT=$PORT_INSIDE -d -p $PORT_OUTSIDE:$PORT_INSIDE -v $PWD/../www:/var/www/html docker_silex_skeleton:latest
 
 echo ">>>> Tailing logs"
-docker logs -f game_of_life
+docker logs -f docker_silex_skeleton
